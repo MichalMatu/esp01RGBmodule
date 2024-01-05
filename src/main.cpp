e4c244c1257777725922fa9694f3c1a2bdf55ced
MichalMatu <michal.matuszewski1984@gmail.com>
2024-01-05 00:08:38 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <Arduino.h>
+#include "FastLED.h"
+// Fastled constants
+#define DATA_PIN 2
+#define COLOR_ORDER GRB
+#define NUM_LEDS 256
+#define LED_TYPE WS2812B
+#define BRIGHTNESS 64
+CRGB leds[NUM_LEDS];
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 1 // Built in LED
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+  pinMode(LED_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void anim1()
+{
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  digitalWrite(LED_PIN, LOW); //  Turn the LED on
+  delay(200);
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+  delay(200);
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  digitalWrite(LED_PIN, HIGH); //  Turn the LED off
+  delay(200);
+  fill_solid(leds, NUM_LEDS, CRGB::White);
+  FastLED.show();
+  delay(200);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  anim1();
 }
